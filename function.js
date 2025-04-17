c873e17b6a5860a66c3a9d90caf98ea912505f0e
RHHEN122 <rokyhenderson222@gmail.com>
2025-04-17 13:15:26 +1200

    Made a Front Page. You can still call the api from raw.html tho.

--- function.js
+++ function.js
@@ -1,5 +1,5 @@
 // Freely made for your content! By Roky Henderson.
 var pfps = Array("1", "2", "3", "4", "5");                      // Makes an array of all the pfps
 var pfpofchoice = pfps[Math.floor(Math.random()*pfps.length)];  // Choses an array randomly
-console.log(pfpofchoice)                                        // Prints the number to the console (for debugging purposes)
-window.location.href=("media/" + pfpofchoice + ".png")          // Goes to the location of the random image
+console.log(pfpofchoice);                                       // Prints the number to the console (for debugging purposes)
+window.location.href=("media/" + pfpofchoice + ".png");         // Goes to the location of the random image
